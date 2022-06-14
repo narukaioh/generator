36bc3b071bb0ce8f5838704e69124327d510d9aa
Ju Dantas <jucienyds@gmail.com>
2022-06-14 17:11:11 -0300

    feat: funcao de nome

--- gerador.js
+++ gerador.js
@@ -4,6 +4,7 @@
 // Appearance: Fit
 const rollDice = (x) => Math.floor(Math.random() * x) + 1
 
+const d2 = () => rollDice(2)
 const d6 = () => rollDice(6)
 const d8 = () => rollDice(8)
 const d10 = () => rollDice(10)
@@ -32,6 +33,25 @@ const ancestry = () => {
         "Tiefling"]
 
     return database[number]
+}
+
+const name = () => {
+    const type = gender()
+    const number = d50() - 1
+    const nonbinary = d2()
+
+    const femaleNames = []
+    const maleNames = []
+
+    if (type === 'Female') {
+        return femaleNames[number]
+    }
+
+    if (type === 'Male') {
+        return maleNames[number]
+    }
+
+    return (nonbinary === 1) ? femaleNames[number] : maleNames[number]
 }
 
 console.log(ancestry())
